3f707bbb2ace97cbba71760c88b8a01e8f99ea82
tomarmayankk <mayanktomarpc@gmail.com>
2024-01-01 11:58:56 +0530

    added a new file

---
+++ Arrays/A1.java
@@ -0,0 +1,13 @@
+//java program to practice arrays 
+package Arrays;
+
+public class A1 {
+    public static void main(String[] args){
+        int[] arr = new int[4];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        System.out.println(arr);
+    }
+    
+}
